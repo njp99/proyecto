c54afca8ad5f7f5eeb912e7b87ec160f17849862
njp99 <124849932+njp99@users.noreply.github.com>
2023-02-09 19:03:58 -0300

    Add files via upload

---
+++ argentinaprograma/js/main.js
@@ -0,0 +1,32 @@
+/**document.getElementById("boton").onclick = function(){
+    console.log("no se que poner");
+    document.getElementById("titulo").innerHTML = "cambio";
+}
+
+document.getElementById("botonc").addEventListener("click", function() {
+      document.getElementById("titulo").innerHTML = "cambio";
+})
+
+
+document.getElementById("botoncolor").addEventListener("click", function () {
+    document.body.style.background = "#335F77";
+})*/
+/**document.getElementById("item").addEventListener("click", function() {
+    document.getElementById("ingles").innerHTML = "Ingles Avanzado";
+})
+document.getElementById("item").addEventListener("click", function() {
+    document.getElementById("office").innerHTML = "Paquete Office";
+})
+document.getElementById("item").addEventListener("click", function() {
+    document.getElementById("java").innerHTML = "JAVA";
+})*/
+
+document.getElementById("contacto").addEventListener("click", function() {
+    document.getElementById("email").innerHTML = "Email";
+})
+document.getElementById("contacto").addEventListener("click", function() {
+    document.getElementById("ig").innerHTML = "Instagram";
+})
+document.getElementById("contacto").addEventListener("click", function() {
+    document.getElementById("wsp").innerHTML = "221-576847";
+})
